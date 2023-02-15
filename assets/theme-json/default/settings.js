a37c971798eb61e27e66b0d83b9c17f4916345c2
Hammad <hmdshfq@outlook.com>
2023-02-15 21:12:57 +0100

    Add settings and styles

--- assets/theme-json/default/settings.js
+++ assets/theme-json/default/settings.js
@@ -1,3 +1,160 @@
 export default {
-    
-}
+    "layout": {
+        "contentSize": "840px",
+        "wideSize": "1100px"
+    },
+    color: {
+        custom: false,
+        customGradient: false,
+        palette: [
+            {
+                slug: 'primary',
+                color: '#0d3b66',
+                name: 'Primary Blue',
+            },
+            {
+                slug: 'secondary',
+                color: '#ee964b',
+                name: 'Secondary',
+            },
+            {
+                slug: 'white',
+                color: '#ffffff',
+                name: 'White',
+            },
+            {
+                slug: 'black',
+                color: '#333333',
+                name: 'Black',
+            },
+        ],
+        gradients: [
+            {
+                slug: 'gradient',
+                gradient: 'linear-gradient(45deg, var(--wp--preset--color--secondary), var(--wp--preset--color--primary))',
+                name: 'Gradient',
+            },
+        ],
+    },
+    typography: {
+        customFontSize: false,
+        dropCap: false,
+        fontWeight: false,
+        fontStyle: false,
+        textTransform: false,
+        textDecoration: false,
+        fontFamilies: [
+            {
+                fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell, "Helvetica Neue",sans-serif',
+                slug: 'sans-serif',
+                name: 'Sans Serif',
+            },
+            {
+                fontFamily: '"Iowan Old Style", "Apple Garamond", Baskerville, "Times New Roman", "Droid Serif", Times, "Source Serif Pro", serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+                slug: 'serif',
+                name: 'Serif',
+            },
+            {
+                fontFamily: 'Menlo, Consolas, Monaco, "Liberation Mono", "Lucida Console", monospace',
+                slug: 'mono',
+                name: 'Monospace',
+            },
+        ],
+        fontSizes: [
+            {
+                slug: 'base',
+                size: '1rem',
+                name: 'base',
+            },
+            {
+                slug: 'lg',
+                size: '1.125rem',
+                name: 'lg',
+            },
+            {
+                slug: 'xl',
+                size: '1.25rem',
+                name: 'xl',
+            },
+            {
+                slug: '2xl',
+                size: '1.5rem',
+                name: '2xl',
+            },
+            {
+                slug: '3xl',
+                size: '1.875rem',
+                name: '3xl',
+            },
+            {
+                slug: '4xl',
+                size: '2.25rem',
+                name: '4xl',
+            },
+            {
+                slug: '5xl',
+                size: '3rem',
+                name: '5xl',
+            },
+            {
+                slug: '6xl',
+                size: '3.75rem',
+                name: '6xl',
+            },
+            {
+                slug: '7xl',
+                size: '4rem',
+                name: '7xl',
+            },
+        ],
+    },
+    custom: {
+        typography: {
+            'line-height': {
+                none: 1,
+                tight: 1.25,
+                snug: 1.375,
+                normal: 1.75,
+                relaxed: 1.625,
+                loose: 2,
+            },
+        },
+    },
+    blocks: {
+        'core/button': {
+            border: {
+                color: false,
+                radius: false,
+                style: false,
+                width: false,
+            },
+            color: {
+                text: false,
+                background: false,
+                customDuotone: false,
+                customGradient: false,
+                defaultDuotone: false,
+                defaultGradients: false,
+                duotone: [],
+                gradients: [],
+                palette: [],
+            },
+            typography: {
+                fontSizes: [],
+                fontFamilies: [],
+            },
+        },
+        'core/heading': {
+            color: {
+                text: false,
+                background: false,
+            },
+            typography: {
+                fontSizes: [],
+                fontFamilies: [],
+                letterCase: false,
+                letterSpacing: false,
+            },
+        },
+    },
+};
--- assets/theme-json/default/styles.js
+++ assets/theme-json/default/styles.js
@@ -1,3 +1,73 @@
 export default {
-    
-}
+    typography: {
+        fontSize: 'var(--wp--preset--font-size--base)',
+    },
+    elements: {
+        h1: {
+            typography: {
+                fontSize: 'var(--wp--preset--font-size--7-xl)',
+                fontFamily: 'var(--wp--preset--font-family--sans-serif)',
+                fontWeight: '700',
+                lineHeight: 'var(--wp--custom--typography--line-height--tight)',
+            },
+        },
+        h2: {
+            typography: {
+                fontSize: 'var(--wp--preset--font-size--6-xl)',
+                fontFamily: 'var(--wp--preset--font-family--sans-serif)',
+                fontWeight: '700',
+                lineHeight: 'var(--wp--custom--typography--line-height--tight)',
+            },
+        },
+        h3: {
+            typography: {
+                fontSize: 'var(--wp--preset--font-size--5-xl)',
+                fontFamily: 'var(--wp--preset--font-family--sans-serif)',
+                fontWeight: '300',
+                lineHeight: 'var(--wp--custom--typography--line-height--tight)',
+            },
+        },
+        h4: {
+            typography: {
+                fontSize: 'var(--wp--preset--font-size--4-xl)',
+                fontFamily: 'var(--wp--preset--font-family--serif)',
+                fontWeight: '700',
+                lineHeight: 'var(--wp--custom--typography--line-height--snug)',
+            },
+        },
+        h5: {
+            typography: {
+                fontSize: 'var(--wp--preset--font-size--3-xl)',
+                fontFamily: 'var(--wp--preset--font-family--serif)',
+                fontWeight: '700',
+                lineHeight: 'var(--wp--custom--typography--line-height--snug)',
+            },
+        },
+        h6: {
+            typography: {
+                fontSize: 'var(--wp--preset--font-size--2-xl)',
+                fontFamily: 'var(--wp--preset--font-family--sans-serif)',
+                fontWeight: '300',
+                lineHeight: 'var(--wp--custom--typography--line-height--normal)',
+            },
+        },
+    },
+    link: {
+        color: {
+            text: 'var(--wp--preset--color--primary)',
+        },
+        ':hover': {
+            color: {
+                text: 'var(--wp--preset--color--secondary)',
+            },
+        },
+    },
+    blocks: {
+        'core/button': {
+            color: {
+                text: 'var(--wp--preset--color--white)',
+                background: 'var(--wp--preset--color--primary)',
+            },
+        },
+    },
+};
